ddb02424c02573c91fd45d91997c6ccdd4e89b1b
Renê Soares <50717882+reneszera@users.noreply.github.com>
2019-06-06 14:42:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/* Define a página atual pela URL */
+$pagina = 'home';
+ 
+if(isset($_GET['pagina'])){
+    $pagina = $_GET['pagina'];
+}
+ 
+/* Carrega o header.php */
+include 'header.php';
+ 
+/* Carrega a página escolhida pelo usuário */
+switch ($pagina) {
+    case 'equipe':
+        include 'equipe.php';
+        break;
+ 
+    default:
+        include '/curso/home.php';
+        break;
+}
+ 
+/* Carrega o footer.php */
+include 'footer.php';
